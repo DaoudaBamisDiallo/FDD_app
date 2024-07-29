f7decfb1d66361a70f5d590e7cfcdcdcf2690486
bamis <daoubamis66@gmail.com>
2024-07-29 21:19:08 +0200

    Correction du nom chargement du lien

--- F2_Diabete_app.py
+++ F2_Diabete_app.py
@@ -10,8 +10,8 @@ import sklearn
 #-------------modelisation et deployement----------------------------
 
 # description de l'application
-st.image("datasets_bd/images/presnation.webp")
 st.title("Bienvenue à Fast Diabete Detection")
+st.image("datasets_bd/images/presnation.webp")
 st.header("Réalisée par : AI-Data_Consulting Group")
 st.markdown(("FFD est une application est conçcue pour détecter très rapidement le diabete chez les femmes"))
 
@@ -32,7 +32,7 @@ def inference(Glucose,BMI,Age,DiabetesPedigreeFunction,BloodPressure,Pregnancies
     return diabetique
 # saisie des iinformations du patience
         
-st.header("Informations du patiente")
+st.header("Informations du patient")
 col1,col2 = st.columns(2)
 with col1 : 
     Glucose = st.number_input(label="Taus du glucose",min_value=0.0,max_value=1.0,value=0.621212)
@@ -45,18 +45,18 @@ with col2 :
     Pregnancies = st.number_input(label="Nombre de grossse",min_value=0.0,max_value=1.0,value=0.205882)
 
     # axamianation du patient
-col3,col4 = st.columns(2)
-with col3 : 
-    if st.button('Examiner le patient',type='secondary') :
-        resultat= inference(Glucose,BMI,Age,DiabetesPedigreeFunction,BloodPressure,Pregnancies)
-        if resultat[0] == 1:
-            st.warning("Diabetique")
-        elif resultat[0] == 0:
-            st.success("Non diabetique")
-        else :
-            st.error("Le résultat de l'examen inconnu merci de bien saisir les information ou de consulter le médecin pour plus de detail")
-with col4 :
-    url ="https://ettienyann-diabete-diabete-tr9slg.streamlit.app/"
-    st.button("[Retour à analyse](%s)" % url,type="primary")
+# col3,col4 = st.columns(2)
+# with col3 : 
+if st.button('Examiner le patient',type='secondary') :
+    resultat= inference(Glucose,BMI,Age,DiabetesPedigreeFunction,BloodPressure,Pregnancies)
+    if resultat[0] == 1:
+        st.warning("Diabetique")
+    elif resultat[0] == 0:
+        st.success("Non diabetique")
+    else :
+        st.error("Le résultat de l'examen inconnu merci de bien saisir les information ou de consulter le médecin pour plus de detail")
+# with col4 :
+#     url ="https://ettienyann-diabete-diabete-tr9slg.streamlit.app/"
+#     st.button("[Retour à analyse](%s)" % url,type="primary")
 
         
